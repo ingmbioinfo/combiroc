f264bf1f634d53dd5f2bf75748b3c6da51217a77
Ivan Ferrari <ferrari@ingm.ad>
2023-04-13 10:44:24 +0200

    test-load_unclassified_data.R

--- tests/testthat/test-load_unclassified_data.R
+++
@@ -1,7 +0,0 @@
-test_that("An unclassified dataframe is read", {
-  expect_s3_class(unc_data, "data.frame")
-})
-
-test_that("the name of second column is enforced to Class", {
-  expect_type(unc_data[,1], "character")
-})
