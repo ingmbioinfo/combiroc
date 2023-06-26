f229684974268a1130760cbb1e9e0b1e943c8d9d
Riccardo Rossi <rossiriccardo@ingm.ad>
2023-06-26 18:29:38 +0200

    details in datasets descriptions

--- R/demo_data.R
+++ R/demo_data.R
@@ -1,7 +1,6 @@
-#' Combiroc built-in demo data (proteomics data from Zingaretti et al. 2012 - PMC3518104)
+#' A demo dataset with proteomics markers
 #'
-#' A dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH).
-#' Samples have been clinically diagnosed as “abnormal” (class A) or "normal" (class B).
+#' Combiroc built-in demo data (proteomics data from Zingaretti et al. 2012 - PMC3518104). A dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH). Samples have been clinically diagnosed as “abnormal” (class A) or "normal" (class B).
 #'
 #'
 #' @format A data frame with 170 rows and 7 variables:
@@ -13,6 +12,5 @@
 #'   \item{Marker3}{the signal intensity value of Marker3}
 #'   \item{Marker4}{the signal intensity value of Marker4}
 #'   \item{Marker5}{the signal intensity value of Marker5}
-
 #' }
 "demo_data"
--- R/demo_unclassified_data.R
+++ R/demo_unclassified_data.R
@@ -1,7 +1,6 @@
-#' Combiroc built-in unclassified demo data
+#' A demo dataset with unlabeled proteomics markers
 #'
-#' A dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH).
-#' This dataset has been obtained by randomly picking 20 samples from demo_data without their classification.
+#' Combiroc built-in unclassified demo dataA dataset containing signal intensity values of a 5-marker signatures for Autoimmune Hepatitis (AIH). This dataset has been obtained by randomly picking 20 samples from demo_data without their classification.
 #'
 #'
 #' @format A data frame with 20 rows and 7 variables:
@@ -12,6 +11,5 @@
 #'   \item{Marker3}{the signal intensity value of Marker3}
 #'   \item{Marker4}{the signal intensity value of Marker4}
 #'   \item{Marker5}{the signal intensity value of Marker5}
-
 #' }
 "demo_unclassified_data"
