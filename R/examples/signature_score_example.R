ff23d049af438928cd15297c3814d73423db3d59
Riccardo Rossi <rossiriccardo@ingm.ad>
2023-06-29 00:21:45 +0200

    deleted signature_score() from main functions

--- R/examples/signature_score_example.R
+++
@@ -1,6 +0,0 @@
-cbmc # combiroc built-in Seurat object for cbmc dataset 
-
-nk_genes # NK specific gene signature found with Seurat protocol
-
-# To compute the gene signature score of a signature according to DellaChiara et al.
-NK_sig_cbmc <- signature_score(SeuratObj = cbmc, geneset = nk_genes)
